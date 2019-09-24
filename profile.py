96f9199229930cf26194d88327670242f95059f8
Jacob Goudy <43557372+jacobgoudy@users.noreply.github.com>
2019-09-24 10:15:04 -0400

    Update profile.py

--- profile.py
+++ profile.py
@@ -23,24 +23,24 @@ prefixForIP = "192.168.1."
 link = request.LAN("lan")
 
 for i in range(3):
-    if i == 0:
-        node = request.XenVM("head")
-        node.routable_control_ip = "true"
-    else:
-        node = request.XenVM("compute-" + str(i))
-        node.cores = 4
-        node.ram = 4096
+  if i == 0:
+    node = request.XenVM("head")
+    node.routable_control_ip = "true"
+  else:
+    node = request.XenVM("compute-" + str(i))
+    node.cores = 4
+    node.ram = 4096
     
-    node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU18-64-STD"
+  node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU18-64-STD"
   
-    iface = node.addInterface("if" + str(i))
-    iface.component_id = "eth1"
-    iface.addAddress(pg.IPv4Address(prefixForIP + str(i + 1), "255.255.255.0"))
-    link.addInterface(iface)
+  iface = node.addInterface("if" + str(i))
+  iface.component_id = "eth1"
+  iface.addAddress(pg.IPv4Address(prefixForIP + str(i + 1), "255.255.255.0"))
+  link.addInterface(iface)
 
-    if i == 0:
-        node.addService(pg.Execute(shell="sh", command="sudo chmod 755 /local/repository/install_mpi.sh"))
-        node.addService(pg.Execute(shell="sh", command="sudo /local/repository/install_mpi.sh"))
+  if i == 0:
+    node.addService(pg.Execute(shell="sh", command="sudo chmod 755 /local/repository/install_mpi.sh"))
+    node.addService(pg.Execute(shell="sh", command="sudo /local/repository/install_mpi.sh"))
   
 # Print the RSpec to the enclosing page.
 pc.printRequestRSpec(request)
